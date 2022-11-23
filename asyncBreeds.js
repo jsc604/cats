c7b92c820f962788ce786f49b75ea023f4c40a2c
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-22 18:23:54 -0800

    initiated and completed testing of cats with mocha and chai

---
+++ asyncBreeds.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    functionToRunWhenThingsAreDone(data);
+  });
+};
+
+module.exports = breedDetailsFromFile;
